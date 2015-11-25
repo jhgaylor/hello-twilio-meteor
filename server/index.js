b05683380163f831454e47494cae567969985580
Jake Gaylor <jhgaylor@gmail.com>
2015-11-25 13:03:28 -0700

    have server retrieve new messages every minute

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ var twilioClient = new Twilio({
   sid: Meteor.settings.TWILIO.SID,
   token: Meteor.settings.TWILIO.TOKEN
 });
+var getTwilioMessages = Meteor.wrapAsync(twilioClient.client.messages.list, twilioClient.client.messages);
 
 Meteor.methods({
   'sendSMS': function (opts) {
@@ -26,3 +27,26 @@ Meteor.methods({
     return result;
   }
 });
+
+function updateMessages () {
+  getTwilioMessages(function (err, data) {
+    if (err) {
+      console.warn("There was an error getting data from twilio", err);
+      return
+    }
+    data.messages.forEach(function (message) {
+      if (SMS.find({sid: message.sid}).count() > 0) {
+        return;
+      }
+      if (message.from === Meteor.settings.TWILIO.FROM) {
+        message.type = "outgoing";
+      } else {
+        message.type = "incoming";
+      }
+      SMS.insert(message);
+    });
+  });
+}
+
+updateMessages();
+Meteor.setInterval(updateMessages, 60000);
